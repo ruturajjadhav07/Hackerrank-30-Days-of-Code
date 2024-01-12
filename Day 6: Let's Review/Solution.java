64a0c89edd51476efca1a8d7a1cdf7788a4663ae
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-12 23:02:47 +0530

    Update Solution.java

--- Day 6: Let's Review/Solution.java
+++ Day 6: Let's Review/Solution.java
@@ -1,1 +1,37 @@
 
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+
+        Scanner sc = new Scanner(System.in);
+  
+        int T = sc.nextInt();
+        sc.nextLine();
+        for (int i = 1; i <= T; i++) {
+      
+            String str = sc.nextLine();
+
+            char[] str1 = str.toCharArray();
+
+            for (int j = 0; j < str.length(); j++) {
+                if (j % 2 == 0) {
+                    System.out.print(str1[j]);
+                }
+            }
+            System.out.print(" ");
+
+            for (int j = 0; j < str.length(); j++) {
+                if (j % 2 == 1) {
+                    System.out.print(str1[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
