7e4aaf9cfdb3db2cca1866c0a0ad4ed0cb143315
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-13 20:51:43 +0530

    Create Solution.java

---
+++ Day 7: Arrays/Solution.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int Array[] = new int[N];
+        for (int i = 0; i < N; i++) {
+
+            Array[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            int reverseArray = N - i - 1;
+            System.out.print(Array[reverseArray] + " ");
+        }
+    }
+}
