ab001221805509a06f99f9919492c21c89cca50e
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-01-15 19:07:07 +0530

    Create Solution1.java

---
+++ Day 9: Recursion 3/Solution1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int factor = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            factor = factor * i;
+           System.out.println(factor);
+        }
+
+        System.out.println("Factorial of " + num + " is: " + factor);
+        System.out.println(factor);
+    }
+}
